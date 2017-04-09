7957ad402e1640c729be61f75be0d4e5be55a540
lztz0022 <lztz0022@126.com>
2017-04-10 00:12:53 +0800

    def func

--- hello.py
+++ hello.py
@@ -1,4 +1,15 @@
-n1=int(input('n1='))
-n2=int(input('n2='))
-print('n1=',n1,'=',hex(n1))
-print('%s%s%s%s' %('n2=',n2,'=',hex(n2)))
+import math
+
+def quadratic(a,b,c):
+    x1=float((-b+math.sqrt(b*b-4*a*c))/(2*a))
+    x2=float((-b-math.sqrt(b*b-4*a*c))/(2*a))
+    return x1,x2
+
+a=int(input('%s' %'a='))
+b=int(input('%s' %'b='))
+c=int(input('c='))
+if b**2<4*a*c:
+    print('方程：',a,'x^2+',b,'x+',c,'=0无解')
+else:
+    x1, x2=quadratic(a,b,c)
+    print('%s%s%s%s%s%s%s%.1f %.1f' %('方程：',a,'x^2',b,'x',c,'=0的解是：',x1,x2))
