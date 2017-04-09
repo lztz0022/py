18dd2cfee36ad96177f74b2bb83eb15a3af00043
lztz0022 <lztz0022@126.com>
2017-04-09 22:22:49 +0800

    loop

---
+++ hello.py
@@ -0,0 +1,16 @@
+L=['tom','lisa','adam']
+for i in L:
+    print('%s, %s!' %('Hello',i))
+print('==============')
+a=0
+while a<3:
+    print('%s, %s!' %('Hello',L[a]))
+    a=a+1
+print('=============================')
+for b in range(3):
+    print('%s, %s!' %('Hello',L[b]))
+
+x=97
+while x<102:
+    print('%s %s' %('Hello,', chr(x)))
+    x=x+1
